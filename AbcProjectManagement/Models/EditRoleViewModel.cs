f39fafcf7cf4a70ab0bce3021d821a055dc00e3e
haksuman <51818946+haksuman@users.noreply.github.com>
2022-11-04 21:28:32 +0300

    All user list management update

---
+++ AbcProjectManagement/Models/EditRoleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AbcProjectManagement.Models
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+
+        public string Id { get; set; }
+        [Required(ErrorMessage ="Role name is required")]
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; }
+    }
+}
